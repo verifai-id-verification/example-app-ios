ed38a8325958b6bdfd4aa82f1dc0b4a6c338daf1
Richard Chirino <gitkraken@giantrobot.nl>
2020-09-29 11:58:40 +0200

    Example app now also shows result images

---
+++ verifai-example/ScanImagesViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ScanImagesViewController.swift
+//  verifai-example
+//
+//  Created by Richard Chirino on 29/09/2020.
+//  Copyright © 2020 Gert-Jan van Ginkel. All rights reserved.
+//
+
+import UIKit
+import VerifaiCommons
+
+class ScanImagesViewController: UIViewController {
+
+    @IBOutlet var frontImageView: UIImageView!
+    @IBOutlet var backImageView: UIImageView!
+    
+    /// The document result being shown
+    var result: VerifaiResult?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Scan result images"
+        frontImageView.image = result?.frontImage
+        backImageView.image = result?.backImage
+
+    }
+
+}
